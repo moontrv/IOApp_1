692acf996a6931d0b00beab0cbf73a382d9c9479
mn <pesrobot@gmail.com>
2016-06-06 13:11:23 +0300

    90% completed

---
+++ ParseStarterProject-Swift/ParseStarterProject/cell.swift
@@ -0,0 +1,18 @@
+//
+//  cell.swift
+//  ParseStarterProject-Swift
+//
+//  Created by mn on 06/06/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+class cell: UITableViewCell {
+
+    @IBOutlet var postedImage: UIImageView!
+    
+    @IBOutlet var username: UILabel!
+
+    @IBOutlet var message: UILabel!
+}
